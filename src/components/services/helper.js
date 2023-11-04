04c75daad5b9a721a08bcd14c0660e7f1bc11620
Md Enamul Hoque Marzun <117737824+marzun9620@users.noreply.github.com>
2023-11-04 19:13:31 +0600

    .

--- src/components/services/helper.js
+++ src/components/services/helper.js
@@ -1,3 +1,3 @@
-//const BASE_URL="https://bikreta.onrender.com"
-const BASE_URL="https://localhost:3000"
+const BASE_URL="https://bikreta.onrender.com"
+//const BASE_URL="https://localhost:3000"
 export default BASE_URL;
